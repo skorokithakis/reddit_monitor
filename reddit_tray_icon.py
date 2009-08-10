74e0f723e833ce4d4f9d50ba80a1211757b96d0c
David Keogh <davekeogh@shaw.ca>
2009-08-10 00:50:43 -0700

    TooltipWidget is only used by the EggTrayIcon

--- reddit_tray_icon.py
+++ reddit_tray_icon.py
@@ -348,52 +348,6 @@ class PopupMenu(object):
         self.ui_manager.get_widget("/TrayMenu").popup(None, None, None, button, activate_time)
 
 
-class TooltipWidget(gtk.HBox):
-    
-    app = None
-    
-    def __init__(self, parent):
-        gtk.HBox.__init__(self)
-        
-        self.app = parent
-        
-        icon = gtk.image_new_from_file(REDDIT_ICON)
-        karma_label = gtk.Label()
-        comment_karma_label = gtk.Label()
-        messages_label = gtk.Label()
-        user_label = gtk.Label()
-        
-        karma_label.set_alignment(0, 0.5)
-        comment_karma_label.set_alignment(0, 0.5)
-        messages_label.set_alignment(0, 0.5)
-        
-        user_label.set_markup('<b><big>%s</big></b>' % self.app.username)
-        
-        karma_label.set_markup('Karma: <b>%d</b>' % self.app.karma)
-        comment_karma_label.set_markup('Comment karma: <b>%d</b>' % self.app.comment_karma)
-        
-        if self.app.messages:
-            messages_label.show()
-            if len(self.app.messages) == 1:
-                messages_label.set_markup('New messages: <b>1</b>')
-            else:
-                messages_label.set_markup('New messages: <b>%d</b>' % len(self.app.messages))
-        else:
-            messages_label.set_markup('New messages: <b>0</b>')
-        
-        vbox = gtk.VBox()
-        vbox.set_spacing(4)
-        vbox.pack_start(user_label)
-        vbox.pack_start(karma_label)
-        vbox.pack_start(comment_karma_label)
-        vbox.pack_start(messages_label)
-        
-        self.pack_start(icon)
-        self.pack_start(vbox)
-        self.set_spacing(6)
-        self.show_all()
-
-
 def open_url(url):
     if XDG_OPEN:
         subprocess.call(['xdg-open', url])
