268063ba8d3d85babedb0c87231a15c2f5fc4566
David Keogh <davekeogh@shaw.ca>
2009-09-07 01:55:55 -0700

    Hide some widgets (properly) when we can't use them

--- redditmonitor/ui.py
+++ redditmonitor/ui.py
@@ -95,10 +95,10 @@ class PopupMenu(object):
         self.ui_manager.get_widget('/TrayMenu/Sound').set_active(self.app.options['sound'])
         
         if not self.app.modules['pynotify']:
-            self.ui_manager.get_widget('/TrayMenu/Notify').set_visible(False)
+            self.ui_manager.get_widget('/TrayMenu/Notify').hide()
         
         if not self.app.modules['gnome']:
-            self.ui_manager.get_widget('/TrayMenu/Sound').set_visible(False)
+            self.ui_manager.get_widget('/TrayMenu/Sound').hide()
     
     def popup(self, widget, button, activate_time, data=None):
         self.ui_manager.get_widget('/TrayMenu').popup(None, None, None, button, activate_time)
